17f01e4794146532a4026b7ce1f186605fb38490
Denis <denisromashov95@gmail.com>
2016-09-24 23:36:35 +0300

    add

---
+++ Java/Rush/Copy_file.java
@@ -0,0 +1,23 @@
+//Копирование файла c:\data.txt в файл c:\result.txt
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopy
+{
+    public static void main(String[] args) throws IOException
+    {
+        FileInputStream  fileInputStream = new FileInputStream("c:\\data.txt");
+        FileOutputStream  fileOutputStream = new FileOutputStream("c:\\result.txt");
+
+        while (fileInputStream.available() > 0)
+        {
+            int data = fileInputStream.read();
+            fileOutputStream.write(data);
+        }
+
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+}
