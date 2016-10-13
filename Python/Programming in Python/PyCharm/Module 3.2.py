ddfeb4a9bc5f0e1a238432b51ebdf4e53e69e012
Denis <denisromashov95@gmail.com>
2016-10-14 00:38:40 +0300

    add

--- Python/Programming in Python/PyCharm/Module 3.2.py
+++ Python/Programming in Python/PyCharm/Module 3.2.py
@@ -4,4 +4,20 @@ basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana' }
 print(basket) #{'apple', 'orange', 'banana', 'pear'}
 print('orange' in basket) #True
 print('python' in basket) #False
+'''
+Операции с множествами
+s.add(element)
+s.remove(element) #при отсутствие элемента возникнит ошибка
+s.discard(element) #удаляет, если элемента нет, ощибки не возникает
+s.clear() #удаляет все элементы из множества
+len(s) #число элемнтов в множестве
+'''
+#Перебор элементов множества
+for i in basket:
+    print(i)
+    #Вывод:
+    #banana
+    #orange
+    #pear
+    #apple
 
