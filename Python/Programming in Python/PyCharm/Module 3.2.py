e7e7019dffa3b383d71ebb46c3b0520ae191dd4e
Denis <denisromashov95@gmail.com>
2016-10-14 00:28:23 +0300

    add

--- Python/Programming in Python/PyCharm/Module 3.2.py
+++ Python/Programming in Python/PyCharm/Module 3.2.py
@@ -1,5 +1,7 @@
 #Множества
-
 s = set() #Создание пустого множества
 basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana' }
-print(basket)
+print(basket) #{'apple', 'orange', 'banana', 'pear'}
+print('orange' in basket) #True
+print('python' in basket) #False
+
