245ac2b45dc7154513d4cb274c8d49bc413d9a9d
Denis <denisromashov95@gmail.com>
2017-09-21 11:10:02 +0300

    ADD

---
+++ Java/KeyboardInput.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class KeyboardInput {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+
+        int minimum = min(a, b);
+
+        System.out.println("Minimum = " + minimum);
+    }
+
+
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+}
